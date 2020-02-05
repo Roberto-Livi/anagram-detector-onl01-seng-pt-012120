5f8d74b03c13481e87697384ad692b7dc0437414
Roberto Livi <robertomlivi@outlook.com>
2020-02-05 14:44:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :lop
+
+def initialize(lop)
+  @lop = lop
+end
+
+def match(array)
+array.each do |i|
+  return i if i.chars.sort == lop.chars.sort
+
+ end
+end
+end
