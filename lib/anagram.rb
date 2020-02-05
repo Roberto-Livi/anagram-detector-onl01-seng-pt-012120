878039c452988b3ebb5b3f4d48c3849e8e8b9b94
Roberto Livi <robertomlivi@outlook.com>
2020-02-05 14:50:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,10 @@ def initialize(word)
 end
 
 def match(array)
+new_array = []
 array.detect do |i|
-  i.chars.sort == word.chars.sort ? i : nil
+  i.chars.sort == word.chars.sort ? new_array << i : nil
+  new_array
  end
 end
 end
